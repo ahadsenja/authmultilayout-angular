3de3eb8e3064b8a3ec308c43214abbacef43e90f
ahadsenja <hamdanilm96@gmail.com>
2021-05-18 21:16:16 +0800

    feat: import signup and login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+
 
 const routes: Routes = [
-  {path: '', redirectTo: '/', pathMatch: 'full'}
+  {path: '', redirectTo: '/', pathMatch: 'full'},
+  {path: 'signup', component: SignupComponent},
+  {path: 'login', component: LoginComponent},
 ];
 
 @NgModule({
