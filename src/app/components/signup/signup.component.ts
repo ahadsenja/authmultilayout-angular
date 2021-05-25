b7ab68c2c5618fd86bcfaeff78cea6d0332a079a
ahadsenja <hamdanilm96@gmail.com>
2021-05-26 00:26:43 +0800

    feat: create on submit code function

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../../service/auth.service';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  form: any = {
+    username: null,
+    email: null,
+    password: null
+  }
+  isSuccessful = false
+  isSignUpFailed = false
+  errorMessage = ''
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    const { username, email, password } = this.form;
+
+    this.authService.register(username, email, password).subscribe(data => {
+      console.log(data);
+      this.isSuccessful = true;
+      this.isSignUpFailed = false;
+    }, error => {
+      this.errorMessage = error.error.message;
+      this.isSignUpFailed = true;
+    })
   }
 
 }
