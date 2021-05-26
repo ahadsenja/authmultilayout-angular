9b19154c50be96c0c0af4621a7b1f3b4a6628afe
ahadsenja <hamdanilm96@gmail.com>
2021-05-26 10:04:34 +0800

    feat: adding login and logout condition function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { TokenStorageService } from './service/token-storage.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'auth-angular';
+
+  private roles: string[] = [];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  username?: string;
+
+  constructor(private token: TokenStorageService) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.token.getToken();
+
+    if(this.isLoggedIn) {
+      const user = this.token.getUser();
+      this.roles = user.roles;
+
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+
+      this.username = user.username;
+    }
+  }
+
+  logout(): void {
+    this.token.logout();
+    window.location.reload();
+  }
 }
