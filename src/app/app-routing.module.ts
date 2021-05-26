37f7a86ee1e885e393924652dbb96e28692c3941
ahadsenja <hamdanilm96@gmail.com>
2021-05-26 10:05:10 +0800

    feat: add some of routing component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { BoardAdminComponent } from './components/board-admin/board-admin.component';
+import { BoardModeratorComponent } from './components/board-moderator/board-moderator.component';
+import { BoardUserComponent } from './components/board-user/board-user.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
   {path: 'signup', component: SignupComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'admin', component: BoardAdminComponent},
+  {path: 'mod', component: BoardModeratorComponent},
+  {path: 'user', component: BoardUserComponent},
+  {path: 'profile', component: ProfileComponent},
 ];
 
 @NgModule({
