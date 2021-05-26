5d69bdcd529a08d58d6f666a451c93278fbce166
ahadsenja <hamdanilm96@gmail.com>
2021-05-26 10:05:54 +0800

    feat: add login and logout condition and function

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TokenStorageService } from '../../service/token-storage.service';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  private roles: string[] = [];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  username?: string;
+
+  constructor(private token: TokenStorageService) { }
 
   ngOnInit(): void {
+    this.isLoggedIn = !!this.token.getToken();
+
+    if(this.isLoggedIn) {
+      const user = this.token.getUser();
+      this.roles = user.roles;
+
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+
+      this.username = user.username;
+    }
+  }
+
+  logout(): void {
+    this.token.logout();
+    window.location.reload();
   }
 
 }
