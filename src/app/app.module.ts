dd5395c878c5a4193e3fbdb526c4920c9a0db537
ahadsenja <hamdanilm96@gmail.com>
2021-08-03 00:20:05 +0800

    reupload all file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
-import { NavbarComponent } from './layout/navbar/navbar.component';
+import { NavbarComponent } from './ui/navbar/navbar.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { BoardAdminComponent } from './components/board-admin/board-admin.component';
 import { BoardModeratorComponent } from './components/board-moderator/board-moderator.component';
