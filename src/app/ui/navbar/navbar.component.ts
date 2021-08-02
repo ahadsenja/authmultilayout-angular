141b61030a16e59b815aca33893f399c23492c26
ahadsenja <hamdanilm96@gmail.com>
2021-08-03 00:12:58 +0800

    chore: rename folder

---
+++ src/app/ui/navbar/navbar.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+import { TokenStorageService } from '../../service/token-storage.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  private roles: string[] = [];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  username?: string;
+
+  constructor(private token: TokenStorageService) { }
+
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.token.getToken();
+
+    if(this.isLoggedIn) {
+      const user = this.token.getUser();
+      this.roles = user.roles;
+
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+
+      this.username = user.username;
+    }
+  }
+
+  logout(): void {
+    this.token.logout();
+    window.location.reload();
+  }
+
+}
