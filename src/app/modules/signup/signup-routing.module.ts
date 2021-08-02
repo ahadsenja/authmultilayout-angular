c735b308776914a051bf2093413c301e8dcb4850
ahadsenja <hamdanilm96@gmail.com>
2021-08-03 01:19:23 +0800

    feat: create signup module

---
+++ src/app/modules/signup/signup-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { SignupComponent } from 'src/app/components/signup/signup.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SignupComponent,
+    children: [
+      {
+        path: 'signup',
+        component: SignupComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class SignupRoutingModule { }
